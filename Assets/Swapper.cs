40faf79956c6f40e4c19a52c95c0e70309653c67
Grandbuilder <christopherreay@Christophers-MacBook-Pro.local>
2016-10-29 14:13:10 -0600

    Yo homeys if I broke your nose let me know

---
+++ Assets/Swapper.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Swapper : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		Cursor.visible = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 temp = Input.mousePosition;
+		temp.z = 10f; // Set this to be the distance you want the object to be placed in front of the camera.
+		this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+	}
+}
---
+++ Assets/Turnt.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turnt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
